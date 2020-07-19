8915d67821c98a9572cf1d8dd73928216ab5f318
Adelar <adelarmello8@gmail.com>
2020-07-19 18:38:29 -0300

    Atualização do perfil: O usuário deve poder mudar sua senha.

--- src/modules/users/services/UpdateProfileService.spec.ts
+++ src/modules/users/services/UpdateProfileService.spec.ts
@@ -56,4 +56,59 @@ describe('UpdateProfileService', () => {
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
+
+  it('should be able to update the password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Usuario 01',
+      email: 'usuario01@gmail.com',
+      password: '123456',
+    });
+
+    const updateUser = await updateProfileService.execute({
+      user_id: user.id,
+      name: 'Usuario 02',
+      email: 'usuario02@gmail.com',
+      old_password: '123456',
+      password: '123123',
+    });
+
+    expect(updateUser.password).toBe('123123');
+  });
+
+  // não deve poder atualizar a senha sem a senha antiga
+  it('should not be able to update the password without old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Usuario 01',
+      email: 'usuario01@gmail.com',
+      password: '123456',
+    });
+
+    await expect(
+      updateProfileService.execute({
+        user_id: user.id,
+        name: 'Usuario 02',
+        email: 'usuario02@gmail.com',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  // não deve poder atualizar a senha com senha antiga incorreta
+  it('should not be able to update the password with wrong old password', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'Usuario 01',
+      email: 'usuario01@gmail.com',
+      password: '123456',
+    });
+
+    await expect(
+      updateProfileService.execute({
+        user_id: user.id,
+        name: 'Usuario 02',
+        email: 'usuario02@gmail.com',
+        old_password: 'wrong-old-password',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
 });
--- src/modules/users/services/UpdateProfileService.ts
+++ src/modules/users/services/UpdateProfileService.ts
@@ -24,7 +24,13 @@ class UpdateProfileService {
     private hashProvider: IHashProvider,
   ) {}
 
-  public async execute({ user_id, name, email }: IRequest): Promise<User> {
+  public async execute({
+    user_id,
+    name,
+    email,
+    password,
+    old_password,
+  }: IRequest): Promise<User> {
     const user = await this.usersRepository.findById(user_id);
 
     if (!user) {
@@ -38,6 +44,24 @@ class UpdateProfileService {
     user.name = name;
     user.email = email;
 
+    // Se o password foi informado, mas o old_password não.
+    if (password && !old_password) {
+      throw new AppError(
+        'You need to inform the old password to set a new password.',
+      );
+    }
+    // Password e old_password informados corretos
+    if (password && old_password) {
+      // Comparar a senha antiga está correta
+      const checkOldPassword = await this.hashProvider.compareHash(
+        old_password,
+        user.password,
+      );
+      if (!checkOldPassword) {
+        throw new AppError('Old password does not match.');
+      }
+      user.password = await this.hashProvider.generateHash(password);
+    }
     return this.usersRepository.save(user);
   }
 }
