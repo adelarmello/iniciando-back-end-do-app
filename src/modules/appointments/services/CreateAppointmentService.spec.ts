5278c7de5ebc01e9e66c96b4505db9c717dff0e5
Adelar <adelarmello8@gmail.com>
2020-07-22 19:41:34 -0300

    Regras do agendamento: Não pode agendar em datas que já passaram.

--- src/modules/appointments/services/CreateAppointmentService.spec.ts
+++ src/modules/appointments/services/CreateAppointmentService.spec.ts
@@ -15,8 +15,12 @@ describe('CreateAppointment', () => {
   });
 
   it('should be able to create a new appointment', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
     const appointment = await createAppointment.execute({
-      date: new Date(),
+      date: new Date(2020, 4, 10, 13),
       user_id: '123123',
       provider_id: '123123',
     });
@@ -39,6 +43,21 @@ describe('CreateAppointment', () => {
         date: appoitmnetDate,
         user_id: '123123',
         provider_id: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+
+  // Não pode agendar em datas que já passaram.
+  it('should be able to create an appointment on a past date', async () => {
+    jest.spyOn(Date, 'now').mockImplementationOnce(() => {
+      return new Date(2020, 4, 10, 12).getTime();
+    });
+
+    await expect(
+      createAppointment.execute({
+        provider_id: 'provider_id',
+        user_id: 'user_id',
+        date: new Date(2020, 4, 10, 11),
       }),
     ).rejects.toBeInstanceOf(AppError);
   });
